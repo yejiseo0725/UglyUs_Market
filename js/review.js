b69fc5924e1102612831de831240f910f7d20e33
yeji seo <yejiseo0725@gmail.com>
2022-07-25 12:34:53 +0900

    main slide 작업.

---
+++ js/review.js
@@ -0,0 +1,17 @@
+$(function () {
+
+    // Main Slide
+    $(".slide-box").slick({
+        dots: false,
+        arrows: false,
+        variableWidth: true,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 0,
+        speed: 3000,
+        infinite: true,
+    });
+
+
+
+});
