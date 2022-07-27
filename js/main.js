2c5ae7d2b683f59d56a2df56a69325786982d355
yeji seo <yejiseo0725@gmail.com>
2022-07-27 15:07:24 +0900

    fixed area 만들기

--- js/main.js
+++ js/main.js
@@ -1,22 +1,68 @@
 $(function () {
+
+  // Section2 이들이 못난이가 된 사연  
   $(".slide-con").slick({
-    // dots: li 형태로 버튼을 만들어줌
-    // dots: true,
     arrows: true,
     prevArrow: $(".left-btn"),
     nextArrow: $(".right-btn"),
     slidesToShow: 3,
-    slidesToScroll: 3, // 한번에 슬라이드 시킬 아이템 개수
-    // centerMode: true,
-    // variableWidth: 내가 정한 width 로 넣어줌
+    slidesToScroll: 3,
     variableWidth: true,
-    // autoplay: true,
-    // autoplaySpeed: 3000,
-    // pauseOnHover: true,
-    // focusOnSelect: 내가 클릭한 slide 로 즉시 이동
-    // focusOnSelect: true,
     vertical: false,
-    // initialSlide: 원하는 slide 부터 시작
     initialSlide: 0,
   });
+
+  let imgIndex;
+  $(".small").click(function () {
+    // small-box 에 border 넣기
+    $(this).addClass("active").siblings().removeClass("active");
+    
+    // small-list index 에 맞춰서 main-img 변화
+    const smallImgIndex = $(this).children().attr("src");
+    $(".main-box").children().attr("src", smallImgIndex);
+  });
+
+  // Small-box 에 맞추어 text 변화
+  $(".small-box2").click(function () {
+    $(".desc-jumbo").removeClass("on").siblings().addClass("on");
+  });
+  $(".small-box1").click(function () {
+    $(".desc-standard").removeClass("on").siblings().addClass("on");
+  });
+
+  // $(".section_03 .section-inner").css("border", "3px solid royalblue");
+
+  const main_title_offset = parseInt($(".main .visual .main-title").offset().top);
+  const sec3_title_offset = parseInt($(".section_03 .title-area h3").offset().top);
+  $(window).scroll(function () {
+
+    const scrollTopLocation = parseInt($(this).scrollTop());
+
+    // Fixed area 
+    if (scrollTopLocation > main_title_offset) {
+      $(".fixed-area").css({
+        position: "fixed",
+      });
+    } else {
+      $(".fixed-area").css({
+        position: "absolute",
+      });
+    };
+
+    // Fixed-area Top btn
+    // $(".fixed-area .top-btn").click(function () {
+      
+    // });
+
+    // Section 03 Animation
+    if (scrollTopLocation >= sec3_title_offset) {
+      $(".intro-cards .card").addClass("active");
+    } else {
+      $(".intro-cards .card").removeClass("active");
+    }
+
+
+  });
+
+
 });
