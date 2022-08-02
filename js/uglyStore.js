7c69abc965619f22ecc834bd7e69ad1806a890a1
yeji seo <yejiseo0725@gmail.com>
2022-08-02 17:55:31 +0900

    어글리스토어 JS 처리중,,

--- js/uglyStore.js
+++ js/uglyStore.js
@@ -1,24 +1,38 @@
 $(function () {
+  const mainSlide = $(".slide-area");
 
-    // Main Slide 
-    $(".slide-area").slick({
-        dots: true,
-        arrows: true,
-        prevArrow: $(".left-btn"),
-        nextArrow: $(".right-btn"),
-        variableWidth: true,
-        slidesToShow: 1,
-    });
+  let $currentSlide;
 
-    // Section 01 Slide 
-    $(".slide-articles").slick({
-        dots: true,
-        arrows: true,
-        prevArrow: $(".left-ant"),
-        nextArrow: $(".right-ant"),
-        variableWidth: true,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-    })
+  mainSlide.on("init", function (event, slick) {
+    $currentSlide = slick.currentSlide;
 
+    $(".desc").eq($currentSlide).addClass("active");
+  });
+
+  mainSlide.on("afterChange", function (event, slick, currentSlide, nextSlide) {
+    $currentSlide = slick.currentSlide;
+
+    $(".desc").removeClass("active").eq($currentSlide).addClass("active");
+  });
+
+  // Main Slide
+  mainSlide.slick({
+    dots: true,
+    arrows: true,
+    prevArrow: $(".left-btn"),
+    nextArrow: $(".right-btn"),
+    variableWidth: true,
+    slidesToShow: 1,
+  });
+
+  // Section 01 Slide
+  $(".slide-articles").slick({
+    dots: true,
+    arrows: true,
+    prevArrow: $(".left-ant"),
+    nextArrow: $(".right-ant"),
+    variableWidth: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  });
 });
