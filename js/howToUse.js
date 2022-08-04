94a750b16d724aa1676527868cd76df334c7c84f
yeji seo <yejiseo0725@gmail.com>
2022-08-04 14:38:50 +0900

    [이용방법 페이지] 스크롤 이벤트

--- js/howToUse.js
+++ js/howToUse.js
@@ -1,12 +1,43 @@
 $(function () {
-    
-    // FAQ Slide Toggle
-    $(".faq-accordion .title").click(function () {
-        $(this).toggleClass("active");
-        $(this).parent().siblings().children().removeClass("active");
+  // Section 03-04 Scroll Event
+  const noticeImgOffset = $(".sec_02 .notice-area .notice").offset().top;
+  const s3LastImgOffset = $(".sec_03 .img-area .last-img").offset().top;
+  const s4TitleOffset = $(".sec_04 .text-area").offset().top;
 
-        $(this).next().stop().slideToggle();
-        $(this).parent().siblings().children(".desc").slideUp();
-    });
+  $(window).scroll(function () {
+    const nowScrollTop = $(this).scrollTop();
 
+    if (nowScrollTop >= noticeImgOffset) {
+      $(".sec_03 .text-area").addClass("active");
+      $(".sec_03 .img-area").addClass("active");
+    } else {
+      $(".sec_03 .text-area").removeClass("active");
+      $(".sec_03 .img-area").removeClass("active");
+    }
+
+    if (nowScrollTop >= s3LastImgOffset) {
+      $(".sec_04 .text-area").addClass("active");
+      $(".sec_04 .small-img").addClass("active");
+      $(".sec_04 .big-img").addClass("active");
+    } else {
+      $(".sec_04 .text-area").removeClass("active");
+      $(".sec_04 .small-img").removeClass("active");
+      $(".sec_04 .big-img").removeClass("active");
+    }
+
+    if (nowScrollTop >= s4TitleOffset) {
+      $(".sec_05 article").addClass("active");
+    } else {
+      $(".sec_05 article").removeClass("active");
+    }
+  });
+
+  // FAQ Slide Toggle
+  $(".faq-accordion .title").click(function () {
+    $(this).toggleClass("active");
+    $(this).parent().siblings().children().removeClass("active");
+
+    $(this).next().stop().slideToggle();
+    $(this).parent().siblings().children(".desc").slideUp();
+  });
 });
