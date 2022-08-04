b6a5a7f1d20755afc24de078ed6cf4e35a850c49
yeji seo <yejiseo0725@gmail.com>
2022-08-04 12:07:07 +0900

    [미션 페이지] 스크롤 이벤트 완성

--- js/mission.js
+++ js/mission.js
@@ -10,42 +10,20 @@ $(function () {
   $(window).scroll(function () {
     const nowScrollTop = $(this).scrollTop();
 
+    // section 01
     if (nowScrollTop >= mainOffset && nowScrollTop < s2Offset) {
-      $(".sec_01 img").css({
-        opacity: "1",
-        visibility: "visible",
-        transform: "translateX(0)",
-      });
-      $(".sec_01 .title-area").css({
-        opacity: "1",
-        visibility: "visible",
-        transform: "translateX(0)",
-      });
+      $(".sec_01 img").addClass("active");
+      $(".sec_01 .title-area").addClass("active");
     } else {
-      $(".sec_01 img").css({
-        opacity: "",
-        visibility: "",
-        transform: "",
-      });
-      $(".sec_01 .title-area").css({
-        opacity: "",
-        visibility: "",
-        transform: "",
-      });
+      $(".sec_01 img").removeClass("active");
+      $(".sec_01 .title-area").removeClass("active");
     }
 
+    // section 02
     if (nowScrollTop >= s1Offset && nowScrollTop < s3Offset) {
-      $(".sec_02 .farm-img").css({
-        opacity: "1",
-        visibility: "visible",
-        transform: "translateY(0)",
-      });
+      $(".sec_02 .farm-img").addClass("active");
     } else {
-      $(".sec_02 .farm-img").css({
-        opacity: "",
-        visibility: "",
-        transform: "",
-      });
+      $(".sec_02 .farm-img").removeClass("active");
     }
 
     if (nowScrollTop >= s2Offset && nowScrollTop < s3Offset) {
@@ -54,6 +32,7 @@ $(function () {
       $(".sec_02 .desc-area .desc").removeClass("active");
     }
 
+    // section 03
     const s2DescOffset = $(".sec_02 .desc-area .desc").offset().top;
     const s2DescPOffset = $(".sec_02 .desc-area .desc p").offset().top;
     if (nowScrollTop >= s2DescOffset && nowScrollTop < s4Offset) {
@@ -67,6 +46,7 @@ $(function () {
       $(".sec_03 .img-area").removeClass("active");
     }
 
+    // section 04
     const s3LastImgOffset = $(".sec_03 .img-area img:last-child").offset().top;
     if (nowScrollTop >= s3LastImgOffset && nowScrollTop < s5Offset) {
       $(".sec_04 .text-area").addClass("active");
@@ -80,6 +60,16 @@ $(function () {
     } else {
       $(".sec_04 .img-area .upper-img").removeClass("active");
       $(".sec_04 .img-area .lower-img").removeClass("active");
+    }
+
+    // section 05
+    const s4ImgOffset = $(".sec_04 .img-area").offset().top;
+    if (nowScrollTop >= s4ImgOffset) {
+      $(".sec_05 .text-area").addClass("active");
+      $(".sec_05 .img-area").addClass("active");
+    } else {
+      $(".sec_05 .text-area").removeClass("active");
+      $(".sec_05 .img-area").removeClass("active");
     }
   });
 });
