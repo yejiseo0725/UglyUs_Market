9ea9ea4d14915175f567feb8a9b931899a981e84
Yeji Seo <65063305+yejiseo0725@users.noreply.github.com>
2022-08-10 18:05:14 +0900

    test update

--- js/basket.js
+++ js/basket.js
@@ -63,23 +63,21 @@ $(function () {
 
   // Button -, + 에 따라 변화하는 수량
   $(".item-area .order-item .quantity button").click(function () {
-    const MAX_COUNT = 5;
-    const MIN_COUNT = 1;
-
     const $this = $(this);
-    const $object = $(this).siblings("h6");
+    const $object = $this.siblings("h6");
+    const $parLocation = $this.parent().parent().parent();
     const $objectCssLocation = $object.parent().siblings(".max-quantity");
 
     let $value = parseInt($object.text());
 
     if ($this.val() === "-") {
-      $value = Math.max(--$value, 1);
-      // if ($value > 1) {
-      // $object.text(--$value);
+      if ($value > 1) {
+        $object.text(--$value);
+      }
     } else {
-      $value = Math.min(++$value, 5);
-      // if ($value < 5) {
-      // $object.text(++$value);
+      if ($value < 5) {
+        $object.text(++$value);
+      }
     }
     $object.text($value);
 
@@ -93,70 +91,73 @@ $(function () {
       });
     }
 
-    // $value = Math.min($value, MIN_COUNT);
-    // $value = Math.max($value, MAX_COUNT);
+    // 상품의 금액 찾기
+    let unitPrice = $parLocation
+      .find(".price")
+      .text()
+      .replace(/[^0-9]/g, "");
 
-    // console.log($value);
+    // 수량 저장
+    let quantity = $value;
 
-    amountPrice();
+    // order-item, 상품금액, 수량 보내기
+    amountPrice($parLocation, unitPrice, quantity);
   });
 
-  function amountPrice() {
-    // orderitem1 의 합계
-    const $orderItem1 = $(".order-item1");
-    const $price1 = $orderItem1
-      .find(".price h5")
-      .text()
-      .replace(/[^0-9]/g, ""); // 상품 가격
-    const $value1 = $orderItem1.find(".quantity h6").text(); // 수량
-    const $sum1 = $price1 * $value1; // item 1 합계: 상품 가격 * 수량
-    $orderItem1.find(".sum h5").text($sum1.toLocaleString() + "원");
+  function amountPrice(a, b, c) {
+    // 위에서 가져온 정보들로 계산만 해주기
 
-    // orderitem2 의 합계
-    const $orderItem2 = $(".order-item2");
-    const $price2 = $orderItem2
-      .find(".price h5")
-      .text()
-      .replace(/[^0-9]/g, ""); // 상품 가격
-    const $value2 = $orderItem2.find(".quantity h6").text(); // 수량
-    const $sum2 = $price2 * $value2; // item 2 합계: 상품 가격 * 수량
-    $orderItem2.find(".sum h5").text($sum2.toLocaleString() + "원");
-
-    // orderitem3 의 합계
-    const $orderItem3 = $(".order-item3");
-    const $price3 = $orderItem3
-      .find(".price h5")
-      .text()
-      .replace(/[^0-9]/g, ""); // 상품 가격
-    const $value3 = $orderItem3.find(".quantity h6").text(); // 수량
-    const $sum3 = $price3 * $value3; // item 3 합계: 상품 가격 * 수량
-    $orderItem3.find(".sum h5").text($sum3.toLocaleString() + "원");
-
-    totalPrice($sum1, $sum2, $sum3);
+    a.find(".sum h5").text(b * c);
+    totalPrice();
   }
 
-  function totalPrice(s1, s2, s3) {
-    const totalSum = s1 + s2 + s3;
-    // console.log(totalSum);
+  function totalPrice() {
+    let $orderItem = $(".order-item");
+    let converseUnit = 0;
+    let amountTotal = 0;
+
+    for (let i = 0; i < $orderItem.length; i++) {
+      // 각각의 합계
+      converseUnit = $orderItem
+        .eq(i)
+        .find(".sum h5")
+        .text()
+        .replace(/[^0-9]/g, "");
+
+      // 각각의 합계마다 더해주며 담아주기
+      amountTotal += parseInt(converseUnit);
+    }
 
     const $totalBox = $(".calc-box");
+    const $total = $totalBox
+      .find(".list-amount h6")
+      .text()
+      .replace(/[^0-9]/g, "");
     const $delivery = $totalBox
       .find(".delivery-amount h6")
       .text()
       .replace(/[^0-9]/g, "");
-    const $choosedItemPrice = $(".list-amount h6")
-      .text()
-      .replace(/[^0-9]/g, "");
-    const $discountCalc = $choosedItemPrice * 0.1;
-    // console.log($discountCalc);
-    const $calc = totalSum - $discountCalc + parseInt($delivery);
-    // console.log($calc);
+    const $discount = $total * 0.1;
+    const $calc;
 
-    $totalBox.find(".list-amount h6").text(totalSum.toLocaleString() + "원");
-    $totalBox
-      .find(".discount-amount h6")
-      .text($discountCalc.toLocaleString() + "원");
-    $totalBox.find(".total-amount h5").text($calc.toLocaleString() + "원");
+    console.log($total);
+
+    // const $totalBox = $(".calc-box");
+    // const $delivery = $totalBox
+    //   .find(".delivery-amount h6")
+    //   .text()
+    //   .replace(/[^0-9]/g, "");
+    // const $choosedItemPrice = $(".list-amount h6")
+    //   .text()
+    //   .replace(/[^0-9]/g, "");
+    // const $discountCalc = $choosedItemPrice * 0.1;
+    // const $calc = totalSum - $discountCalc + parseInt($delivery);
+
+    // $totalBox.find(".list-amount h6").text(totalSum.toLocaleString() + "원");
+    // $totalBox
+    //   .find(".discount-amount h6")
+    //   .text($discountCalc.toLocaleString() + "원");
+    // $totalBox.find(".total-amount h5").text($calc.toLocaleString() + "원");
   }
 
   $(".close-area .close-btn").click(function () {
